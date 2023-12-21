7fc5118dae88761028c702b76bdc74dc6ffe8dca
Avibtb <avi.singh.iit01@gmail.com>
2023-12-21 11:13:44 +0530

    Added UserDetails class.

---
+++ src/main/java/com/avinash/login/security/services/UserDetailsSeerviceImpl.java
@@ -0,0 +1,21 @@
+package com.avinash.login.security.services;
+
+import com.avinash.login.model.User;
+import com.avinash.login.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsSeerviceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " +username));
+        return UserDetailsImpl.build(user);
+    }
+}
